175ab13b5f27f723fe20998dd615f6abe076ce95
Netta Ben Eliyahu <nettabenezrils@gmail.com>
2026-03-11 20:33:51 +0200

    add datatbase models (Product, Coupon, Reseller)

---
+++ backend/app/models/coupon.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Numeric, Boolean, String, Enum, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+import enum
+from app.models.product import Product
+
+
+class CouponValueType(enum.Enum):
+    STRING = "STRING"
+    IMAGE = "IMAGE"
+
+
+class Coupon(Product):
+    __tablename__ = "coupons"
+    id = Column(UUID(as_uuid=True),ForeignKey("products.id"), primary_key=True)
+    cost_price = Column(Numeric(10, 2), nullable=False)
+    margin_percentage = Column(Numeric(10, 2), nullable=False)
+    minimum_sell_price = Column(Numeric(10, 2), nullable=False)
+    is_sold = Column(Boolean, default=False)
+    value_type = Column(Enum(CouponValueType), nullable=False)
+    value = Column(String, nullable=False)
+
+    __mapper_args__ = {
+        "polymorphic_identity": "COUPON"
+    }
---
+++ backend/app/models/product.py
@@ -0,0 +1,30 @@
+import enum
+import uuid
+from sqlalchemy import Column, String, Enum, DateTime, func
+from sqlalchemy.dialects.postgresql import UUID
+
+from app.models.base import Base
+
+class ProductType(enum.Enum):
+    COUPON = "COUPON"
+
+class Product(Base):
+    __tablename__ = "products"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    type = Column(Enum(ProductType), nullable=False)
+    image_url = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True),server_default=func.now(), onupdate=func.now())
+
+    __mapper_args__ = {
+        "polymorphic_identity": "PRODUCT",
+        "polymorphic_on": type              
+    }
+
+
+
+
+
+    
